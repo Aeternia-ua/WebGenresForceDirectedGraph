4378c3198044650e3ec69414c7d87cbba0b19bef
Aeternia <gherasymuk@gmail.com>
2016-10-30 17:25:48 +0200

    Fix to show labels at the nides

--- js/index.js
+++ js/index.js
@@ -4,108 +4,126 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-///Setting color scheme
 var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
 var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) {
+        return d.id;
+    }))
     .force("charge", d3.forceManyBody().strength(-150))
-    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(90))
-    .force("x", d3.forceX(width / 2))
-    .force("y", d3.forceY(height / 2))
-    .on("tick", ticked);
-
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-
-var tooltip = d3.select("#tooltip").style("opacity", 0);
-
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
 d3.json(dataURL, function(error, graph) {
-  if (error) throw error;
+    if (error) throw error;
 
-  simulation.nodes(graph.nodes);
-  simulation.force("link").links(graph.links);
+    simulation.nodes(graph.nodes);
+    simulation.force("link").links(graph.links);
 
-  link = link
-    .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link");
+    var link = svg.append("g")
+        .attr("class", "link")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line");
 
-  //Visualizing nodes
-   node = node
-    .data(graph.nodes)
-    .enter().append("circle")
-    .attr("class", "node")
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
 
- //Setting node radius by group value. If 'group' key doesn't exist, set radius to 8
-	 .attr("r", function(d){
-        if(d.hasOwnProperty('group')){
-          return d.group * 2;
-    } else {
-          return 8;
-    }
-   })
+    //Setting node radius by group value. If 'group' key doesn't exist, set radius to 8
+    .attr("r", function(d) {
+            if (d.hasOwnProperty('group')) {
+                return d.group * 2;
+            } else {
+                return 9;
+            }
+        })
+        //Colors by 'group' value
+        .style("fill", function(d) {
+            return color(d.group);
+        })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
 
-  //Colors by 'group' value
-      .attr("fill", function(d) { return color(d.group); })
+    node.append("svg:title")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text(function(d) {
+            return d.id
+        });
+
+    var labels = svg.append("g")
+        .attr("class", "label")
+        .selectAll("text")
+        .data(graph.nodes)
+        .enter().append("text")
+        .attr("dx", 6)
+        .attr("dy", ".35em")
+        .style("font-size", 10)
+        .text(function(d) {
+            return d.id
+        });
+
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
   
-  ///Placing labels at the nodes
-  
-  
-  ////SHOWS TOOLTIP ON MOUSE HOVER
-  .on('mouseover', function(d) {
-      tooltip.transition()
-        .duration(100)
-        .style("opacity", 0.8);
-      var tooltipHtml = "<span>" + d.id + "</span>";
-      tooltip.html(tooltipHtml)
-        .style("left", (d3.event.pageX + 5) + "px")
-        .style("top", (d3.event.pageY - 50) + "px");
-    })
-    .on('mouseout', function() {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity", 0);
-    })
-  
-.call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
- 
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+  function ticked() {
 
-  simulation.force("link")
-      .links(graph.links);
+    link.attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+
+    node
+        .attr("cx", function(d) {
+            return d.x;
+        })
+        .attr("cy", function(d) {
+            return d.y;
+        });
+
+    labels
+        .attr("x", function(d) {
+            return d.x;
+        })
+        .attr("y", function(d) {
+            return d.y;
+        });
+}
   
 });
 
-function ticked() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
- 
-  
-    }
+
 function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
 }
 
 function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 }
 
 function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
 }
